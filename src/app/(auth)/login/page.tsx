6fb9a566b073ca6cef27a35ca8c3f8e7fab94605
Bagas Ferdianto <bagasferdian321@gmail.com>
2025-05-15 16:46:51 +0700

    set routing folder

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login () {
+    return <h1>Ini halaman Login ygy</h1>
+}
---
+++ src/app/voting/page.tsx
@@ -0,0 +1,3 @@
+export default function Voting () {
+    return <h1>Ini adalah halaman Voting atau Page Voting ygy</h1>
+}
