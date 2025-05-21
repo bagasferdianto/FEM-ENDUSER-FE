04fdf2f0d91afc8a2b47d286c56542e07d583bf4
alfianhs <alfianhsofyan7@gmail.com>
2025-05-22 02:48:32 +0700

    fix: implement tailwind config, modify directory pattern, and modify shadcn ui component

--- src/app/(superadmin)/signin/page.tsx
+++ src/app/(superadmin)/signin/page.tsx
@@ -1,3 +1,43 @@
-export default function Signin () {
-    return <h2>Halaman Sign In Superadmin</h2>
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import AuthLayout from "@/components/auth/AuthLayout";
+import Image from "next/image";
+import { Label } from "@/components/ui/label";
+
+export default function SignInPage() {
+  return (
+    <AuthLayout>
+      <div className="w-full h-full flex flex-col justify-between items-center max-w-sm">
+        <div className="self-start">
+          <Image
+            src="/images/pfl-logo-biru-horizontal.png"
+            alt="PFL"
+            width={150}
+            height={50}
+          />
+        </div>
+        <div className="w-full">
+          <div className="flex items-center justify-center mb-4 w-full">
+            <h1 className="text-lg font-bold">Masuk Sebagai Superadmin</h1>
+          </div>
+          <div className="space-y-4 w-full">
+            <div className="w-full text-black">
+              <Label htmlFor="email">Email</Label>
+              <Input placeholder="Masukkan email" type="email" id="email" />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input placeholder="Masukkan password" type="password" id="password" />
+            </div>
+            <Button className="text-white w-full bg-blue-800 hover:bg-blue-900" size={"lg"}>
+              Masuk
+            </Button>
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-center mb-8">
+          <p>© 2024 Pro Futsal League</p>
+        </div>
+      </div>
+    </AuthLayout>
+  );
 }
